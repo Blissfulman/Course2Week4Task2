b6f4378fbb57aa12a16dd248f5b70a841ce0a551
Evgeny <greatly01@ya.ru>
2020-07-11 00:03:02 +0500

    Started 3 task 4 task is done

--- Course2Week4Task2/Code/Animations/CATransitionController.swift
+++ Course2Week4Task2/Code/Animations/CATransitionController.swift
@@ -12,6 +12,49 @@ class CATransitionController: UIViewController {
     @IBOutlet weak var textLabel: UILabel!
 
     @IBAction func swipeHandler(_ sender: UISwipeGestureRecognizer) {
+        
+        let startText = textLabel.text
+        let startTextColor = textLabel.textColor
+        
+//        let movS = {
+            let moveInAnimation = CATransition()
+            self.textLabel.text = "Sliding!"
+            self.textLabel.textColor = .green
+            moveInAnimation.duration = 1.0
+//            moveInAnimation.startProgress = 0
+//            moveInAnimation.endProgress = 1.0
+            moveInAnimation.type = CATransitionType.moveIn
+            moveInAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+            self.textLabel.layer.add(moveInAnimation, forKey: "moveIn")
+//        }
+        
+//        let movF = {
+            let fadeAnimation = CATransition()
+            self.textLabel.text = startText
+            self.textLabel.textColor = startTextColor
+//            fadeAnimation.startProgress = 1.0
+//            fadeAnimation.endProgress = 2.0
+            fadeAnimation.duration = 1.0
+            fadeAnimation.beginTime = 2
+//            fadeAnimation.autoreverses = true
+            fadeAnimation.type = CATransitionType.fade
+            fadeAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+            self.textLabel.layer.add(fadeAnimation, forKey: "fade")
+//        }
+        
+        let animations = [fadeAnimation]//, fadeAnimation]
+        
+        let group = CAAnimationGroup()
+        group.animations = animations
+        group.beginTime = 2
+        textLabel.layer.add(group, forKey: nil)
+        
+//        movS()
+//        movF()
+        
+//        UIView.animate(withDuration: 2, animations: movS, completion: {
+//            (value: Bool) in
+//            UIView.animate(withDuration: 3, delay: 2, animations: movF)
+//        })
     }
-
 }
--- Course2Week4Task2/Code/Animations/UIViewAnimationController.swift
+++ Course2Week4Task2/Code/Animations/UIViewAnimationController.swift
@@ -13,8 +13,17 @@ class UIViewAnimationController: UIViewController {
     @IBOutlet weak var flipButton: UIButton!
     
     @IBAction func animationViewTapHandler(sender: UITapGestureRecognizer) {
+        UIView.animate(withDuration: 1, delay: 0.5, options: .curveEaseInOut, animations: {
+            let dx = self.view.frame.width - self.animationView.frame.width
+            self.animationView.frame = self.animationView.frame.offsetBy(dx: dx, dy: 0)
+            self.animationView.transform = self.animationView.transform.rotated(by: .pi)
+        })
     }
     
     @IBAction func flipButtonTapHandler(sender: UIButton) {
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseInOut, animations: {
+            self.view.transform = self.view.transform.rotated(by: .pi)
+            self.flipButton.transform = self.flipButton.transform.rotated(by: .pi)
+        })
     }
 }
